e168c08418c2982184472e1a14fc7d76fb1dacd0
OskarPodrucki <oskar.podrucki1@gmail.com>
2025-04-15 07:00:37 +0200

    Initialize project structure and add basic page layout

---
+++ src/components/Header/mobilelinks.js
@@ -0,0 +1,8 @@
+export default function Header() {
+	return (
+		<div className='flex justify-center items-center w-4/5 h-[100px] mb-3 bg-amber-100'>
+			<div>header</div>
+			<div>links</div>
+		</div>
+	);
+}
