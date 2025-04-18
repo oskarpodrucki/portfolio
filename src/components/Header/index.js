64eaf3f28f3a2f49d897ba9bd597b9c1eb785072
OskarPodrucki <oskar.podrucki1@gmail.com>
2025-04-18 14:19:49 +0200

    ...

--- src/components/Header/index.js
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-	return (
-		<div className='flex justify-between items-center w-4/5 h-[100px] mb-3 bg-amber-100'>
-			<div>header</div>
-			<div>links</div>
-		</div>
-	);
-}
