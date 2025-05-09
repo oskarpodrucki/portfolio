260e6c309ee96cc112b083c7db2cdd8cdca84bae
OskarPodrucki <oskar.podrucki1@gmail.com>
2025-05-09 18:19:51 +0200

    changes in docs/changelog.txt

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,38 +1,36 @@
 import { Card } from "@/components/ui/card";
 import Link from "next/link";
-import { AnimatedBackground } from "../ui/animated-background";
 
 export default function Navbar() {
 	return (
 		<Card className='flex flex-row justify-between items-center w-[777px] h-[55px] p-5 border-solid border-2'>
-			<AnimatedBackground>
-				
-			</AnimatedBackground>
-			<Link
-				href='/'
-				className='transition duration-150 ease-in-out hover:scale-110 hover:text-green-400'>
-				/
-			</Link>
-			<Link
-				href='/portfolio'
-				className='transition duration-150 ease-in-out hover:scale-110 hover:text-sky-400'>
-				/portfolio
-			</Link>
-			<Link
-				href='/about'
-				className='transition duration-150 ease-in-out hover:scale-110 hover:text-amber-400'>
-				/about
-			</Link>
-			<Link
-				href='/contact'
-				className='transition duration-150 ease-in-out hover:scale-110 hover:text-rose-400'>
-				/contact
-			</Link>
-			<Link
-				href='/blog'
-				className='transition duration-150 ease-in-out hover:scale-110 hover:text-violet-400'>
-				/blog
-			</Link>
+			
+				<Link
+					href='/'
+					className='transition duration-150 ease-in-out hover:scale-110 hover:text-green-400'>
+					/
+				</Link>
+				<Link
+					href='/portfolio'
+					className='transition duration-150 ease-in-out hover:scale-110 hover:text-sky-400'>
+					/portfolio
+				</Link>
+				<Link
+					href='/about'
+					className='transition duration-150 ease-in-out hover:scale-110 hover:text-amber-400'>
+					/about
+				</Link>
+				<Link
+					href='/contact'
+					className='transition duration-150 ease-in-out hover:scale-110 hover:text-rose-400'>
+					/contact
+				</Link>
+				<Link
+					href='/blog'
+					className='transition duration-150 ease-in-out hover:scale-110 hover:text-violet-400'>
+					/blog
+				</Link>
+			
 		</Card>
 	);
 }
